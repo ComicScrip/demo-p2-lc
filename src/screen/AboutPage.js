1a1a4af85d9b9f5d7f137f938dcf4057ca7ba11a
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-07 09:38:46 +0200

    creation About page and the component team member

---
+++ src/screen/AboutPage.js
@@ -0,0 +1,30 @@
+import TeamMember from '../components/TeamMember';
+import styles from './AboutPage.module.css';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Notre équipe</h1>
+      <div className={styles.memberList}>
+        <TeamMember/>
+      
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+/*INFO POUR LES MEMBRES 
+        name="Pierre"
+        bio={'Bio Pierre'}
+        githubLink="https://github.com/ComicScrip"
+
+        name="Nico"
+        bio={'Bio Nico'}
+        githubLink="https://github.com/NycoChavanoud"
+
+        */
