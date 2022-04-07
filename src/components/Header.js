b1519b920c6c1e805e15a1f0103a5930d95253c0
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-07 10:52:50 +0200

    setup links in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
-      <nav>nav</nav>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
     </header>
   );
 }
