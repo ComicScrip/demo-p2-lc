0062ba792e2cd1ecabfb9702942a443a9ff5d1e9
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-07 09:52:09 +0200

    add errors two

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -4,19 +4,15 @@ export default function TeamMember() {
   return (
     <div className={styles.card}>
       <p className={styles.name}>Pierre</p>
+
       <a href="https://github.com/ComicScrip" rel="noreferrer">
-        <img className={styles.avatar} src={`https://github.com/ComicScrip.png`}  />
+        <img
+          className={styles.avatar}
+          src={`https://github.com/ComicScrip.png`}
+        />
       </a>
 
       <p className={styles.bio}>Bio Pierre</p>
     </div>
   );
 }
-
-
-
-
-
-
-
-
